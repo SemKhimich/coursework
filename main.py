2258037763f62304076fabbd7555c9d5dd9c282f
SemyonKhimich <semyonximich@gmail.com>
2019-11-22 22:16:18 +0300

    attempt-x-fully-connected-layers

--- main.py
+++ main.py
@@ -3,26 +3,9 @@ from tensorflow import keras
 import sys
 import inspect
 
-filename = 'dataset2.txt'
+filename = 'dataset_nov_22_all.txt'
 array = np.loadtxt(filename, delimiter=',')
-n = len(array)
-x_train, y_train, x_test, y_test = \
-    array[:5 * n // 6, :-2], array[:5 * n // 6, -2:], \
-    array[5 * n // 6:, :-2], array[5 * n // 6:, -2:]
-for i in range(len(x_train[0])):  # нормализация
-    max_in_column = max(x_train[:, i].max(), x_test[:, i].max())
-    min_in_column = min(x_train[:, i].min(), x_test[:, i].min())
-    x_train[:, i] = (x_train[:, i] - min_in_column) / (max_in_column - min_in_column)
-    x_test[:, i] = (x_test[:, i] - min_in_column) / (max_in_column - min_in_column)
-
-# d = {}
-# for line in array:
-#     d.setdefault(line[-2], 0)
-#     d[line[-2]] += 1
-# for key in d:
-#     print('key = ', key, ': ', d[key] / len(array))
-
-file = open('logs2.txt', 'a', encoding='utf-8')
+file = open('logs_nov_22_all.txt', 'a', encoding='utf-8')
 sys.stdout = file
 print('**************************************************')
 print('**************************************************')
@@ -30,22 +13,37 @@ code_lines = list(inspect.getsourcelines(sys._getframe(0))[0])
 index_from = code_lines.index('# begin\n')
 index_to = code_lines.index('# end\n')
 print(''.join(code_lines[index_from + 1:index_to]))
-
+n = len(array)
+for i in range(len(array[0]) - 2):  # заполняю нулевые значения средним в столбце
+    column = array[:, i:i + 1]
+    mean_in_column = column[column != 0].mean()
+    column[column == 0] = mean_in_column
+for i in range(len(array[0]) - 2):  # нормализация
+    max_in_column = array[:, i].max()
+    min_in_column = array[:, i].min()
+    array[:, i] = (array[:, i] - min_in_column) / (max_in_column - min_in_column)
+xy_train, xy_test = array[:5 * n // 6, :], array[5 * n // 6:, :]
+xy_test_zeros = xy_test[xy_test[:, -1] == 0]  # одна тестовая выборка один признак
+xy_test_ones = xy_test[xy_test[:, -1] == 1]  # вторая другой
 # begin
 model = keras.Sequential([
-    keras.layers.Input(21),
-    keras.layers.Dense(20, activation='relu'),
-    keras.layers.Dense(20, activation='relu'),
-    keras.layers.Dense(20, activation='relu'),
-    keras.layers.Dense(20, activation='relu'),
-    keras.layers.Dense(2, activation='softmax')  # tanh, sigmoid, hard_sigmoid, softsign, relu
-])
+        keras.layers.Input(21),
+        keras.layers.Dense(128, activation='relu'),
+        keras.layers.Dropout(0.2),
+        keras.layers.Dense(64, activation='relu'),
+        keras.layers.Dropout(0.2),
+        keras.layers.Dense(32, activation='relu'),
+        keras.layers.Dense(2, activation='softmax')  # tanh, sigmoid, hard_sigmoid, softsign, relu
+    ])
 model.compile(optimizer='adam',
               loss='binary_crossentropy',  # sparse_categorical_crossentropy, binary_crossentropy, mean_squared_error
               metrics=['accuracy'])
-model.fit(x_train, y_train, epochs=10)
+model.fit(xy_train[:, :21], xy_train[:, 21:], epochs=60, verbose=2)
+test_loss_zeros, test_acc_zeros = model.evaluate(xy_test_zeros[:, :21], xy_test_zeros[:, 21:])
+test_loss_ones, test_acc_ones = model.evaluate(xy_test_ones[:, :21], xy_test_ones[:, 21:])
 # end
-
-test_loss, test_acc = model.evaluate(x_test, y_test)
-print('test_loss:', test_loss, '- test_accuracy:', test_acc)
+print('test_loss_zeros:', test_loss_zeros, '- test_accuracy_zeros:', test_acc_zeros)
+print('test_loss_ones:', test_loss_ones, '- test_accuracy_ones:', test_acc_ones)
+print('total_accuracy:', (test_acc_zeros * len(xy_test_zeros) + test_acc_ones * len(xy_test_ones)) /
+      (len(xy_test_zeros) + len(xy_test_ones)))
 file.close()
--- parse.py
+++ parse.py
@@ -3,7 +3,7 @@ import random
 
 def create_dataset(dataset_numbers):
     filename_input = '{0}/description_data_{0}.txt'
-    rows = set()
+    rows_set = set()
     zero_result_set = {'false', 'no'}
     for num in dataset_numbers:
         with open(filename_input.format(num), 'r', encoding='utf-8') as file_input:
@@ -11,13 +11,19 @@ def create_dataset(dataset_numbers):
                 if line[0] not in '0123456789' or '?' in line:
                     continue
                 row = line.strip().split(',')
-                row[-1:] = ['0', '1'] if row[-1] in zero_result_set else ['1', '0']
-                rows.add(','.join(map(lambda x: str(float(x)), row)))
-    rows2 = list(rows)
+                row[-1:] = ['1', '0'] if row[-1] in zero_result_set else ['0', '1']
+                rows_set.add(','.join(map(lambda x: str(float(x)), row)))
+    rows_list = []
+    for row in rows_set:
+        rows_list.append(row)
+        if row[-3:] == '1.0':
+            rows_list.append(row)
+            rows_list.append(row)
+            rows_list.append(row)
     for i in range(100):
-        random.shuffle(rows2)
-    with open('dataset3.txt', 'w', encoding='utf-8') as file_output:
-        for row in rows2:
+        random.shuffle(rows_list)
+    with open('dataset_nov_22_all.txt', 'w', encoding='utf-8') as file_output:
+        for row in rows_list:
             file_output.write(row + '\n')
 
 
