ce1ffd481a68c52bebff1b51eb7a750d9a0afeec
SemyonKhimich <semyonximich@gmail.com>
2019-12-10 20:44:42 +0300

    trained network

---
+++ testing.py
@@ -0,0 +1,18 @@
+import numpy as np
+from tensorflow import keras
+
+if __name__ == '__main__':
+    model = keras.models.load_model('my_model.h5')
+    mean_var = np.loadtxt('mean_var_info.txt', delimiter=',')
+    file_names = ['4/data_4.txt', '5/data_5.txt']
+    for name in file_names:
+        data_test = np.loadtxt(name, delimiter=',')
+        data_test[:, :-2] = (data_test[:, :-2] - mean_var[0]) / mean_var[1]**0.5
+        data_test_zeros = data_test[data_test[:, -1] == 0]
+        data_test_ones = data_test[data_test[:, -1] == 1]
+        _, test_acc_zeros = model.evaluate(data_test_zeros[:, :-2], data_test_zeros[:, -2:], verbose=0)
+        print('test_acc_zeros: ', test_acc_zeros, sep='')
+        _, test_acc_ones = model.evaluate(data_test_ones[:, :-2], data_test_ones[:, -2:], verbose=0)
+        print('test_acc_ones: ', test_acc_ones, sep='')
+        print('total_acc: ', (test_acc_ones * len(data_test_ones) +
+                              test_acc_zeros * len(data_test_zeros)) / len(data_test))
---
+++ train_and_validation.py
@@ -0,0 +1,83 @@
+import numpy as np
+from tensorflow import keras
+import sys
+import inspect
+import matplotlib.pyplot as plt
+
+
+def print_network_architecture(file):
+    print('**************************************************', file=file)
+    print('**************************************************', file=file)
+    code_lines = list(inspect.getsourcelines(sys._getframe(1))[0])
+    index_from = code_lines.index('    # begin\n')
+    index_to = code_lines.index('    # end\n')
+    print(''.join(code_lines[index_from + 1:index_to]), file=file)
+
+
+def plot_results(history):
+    fig = plt.figure(figsize=(10, 6))
+    sub1 = fig.add_subplot(121)
+    sub1.set_title('График точности')
+    sub1.set_xlabel('Эпоха')
+    sub1.plot(history.history['accuracy'])
+    sub2 = fig.add_subplot(122)
+    sub2.set_title('График функции потерь')
+    sub2.set_xlabel('Эпоха')
+    sub2.plot(history.history['loss'])
+    plt.tight_layout()
+    plt.savefig('graphics/graphic_model_30.png')
+    # plt.show()
+
+
+def get_network_model(m):
+    # begin
+    # model_30
+    return keras.Sequential([
+        keras.layers.Input(m),
+        keras.layers.Dense(200, activation='elu',
+                           kernel_initializer=keras.initializers.RandomNormal(
+                               mean=0.0, stddev=0.1, seed=None)),
+        keras.layers.Dropout(0.15),
+        keras.layers.Dense(100, activation='elu',
+                           kernel_initializer=keras.initializers.RandomNormal(
+                               mean=0.0, stddev=0.1, seed=None)),
+        keras.layers.Dropout(0.15),
+        keras.layers.Dense(50, activation='elu',
+                           kernel_initializer=keras.initializers.RandomNormal(
+                               mean=0.0, stddev=0.1, seed=None)),
+        keras.layers.Dense(2, activation='softmax')
+    ])
+    # end
+
+
+data = np.loadtxt('train_validation_balanced_data.txt', delimiter=',')
+n = len(data)
+data_train, data_validation = data[:5 * n // 6, :], data[5 * n // 6:, :]
+file_logs_output = open('logs.txt', 'a', encoding='utf-8')
+print_network_architecture(file_logs_output)
+
+data_validation_zeros = data_validation[data_validation[:, -1] == 0]
+data_validation_ones = data_validation[data_validation[:, -1] == 1]
+
+model = get_network_model(len(data_train[0]) - 2)
+model.compile(optimizer='nadam', loss='binary_crossentropy',
+              metrics=['accuracy'])
+hist = model.fit(data_train[:, :-2], data_train[:, -2:], epochs=20, verbose=0)
+
+print('train_loss: ', hist.history['loss'][-1],
+      ', train_acc: ', hist.history['accuracy'][-1], file=file_logs_output)
+
+loss, acc_ones = model.evaluate(data_validation_ones[:, :-2], data_validation_ones[:, -2:], verbose=0)
+print('validation_ones_loss: ', loss,
+      ', validation_ones_acc: ', acc_ones, sep='', file=file_logs_output)
+
+loss, acc_zeros = model.evaluate(data_validation_zeros[:, :-2], data_validation_zeros[:, -2:], verbose=0)
+print('validation_zeros_loss: ', loss,
+      ', validation_zeros_acc: ', acc_zeros, sep='', file=file_logs_output)
+
+print('total_acc: ', (acc_ones * len(data_validation_ones) +
+                      acc_zeros * len(data_validation_zeros)) / len(data_validation),
+      file=file_logs_output)
+model.save('my_model.h5')
+file_logs_output.close()
+plot_results(hist)
